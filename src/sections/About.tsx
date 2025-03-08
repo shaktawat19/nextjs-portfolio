d22aadd61b92b11f3ae9d59486299f23b309ab18
shaktawat19 <harshshaktawat.tech@gmail.com>
2025-03-08 15:04:20 +0530

    About section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,169 @@
+import { Card } from '@/components/Card'
+import { SectionHeader } from '@/components/SectionHeader'
+import StarIcon from '@/assets/icons/star.svg'
+import bookImage from '@/assets/images/book-cover.png'
+import Image from 'next/image'
+import JavascriptIcon from '@/assets/icons/square-js.svg'
+import HTMLIcon from '@/assets/icons/html5.svg'
+import CssIcon from '@/assets/icons/css3.svg'
+import ReactIcon from '@/assets/icons/react.svg'
+import ChromeIcon from '@/assets/icons/chrome.svg'
+import GithubIcon from '@/assets/icons/github.svg'
+import { TechIcon } from '@/components/TechIcon'
+import mapImage from '@/assets/images/map.png'
+import smileMemoji from '@/assets/images/memoji-smile.png'
+import { title } from 'process'
+import { CardHeader } from '@/components/CardHeader'
+import { ToolboxItems } from '@/components/ToolboxItems'
+
+const toolboxItems = [
+  {
+    title: 'JavaScript',
+    iconType: JavascriptIcon,
+  },
+  {
+    title: 'React',
+    iconType: ReactIcon,
+  },
+  {
+    title: 'HTML5',
+    iconType: HTMLIcon,
+  },
+  {
+    title: 'CSS3',
+    iconType: CssIcon,
+  },
+  {
+    title: 'Chrome',
+    iconType: ChromeIcon,
+  },
+  {
+    title: 'Github',
+    iconType: GithubIcon,
+  },
+]
+
+const hobbies = [
+  {
+    title: 'Traveling',
+    emoji: '🌍',
+    top: '5%',
+    left: '5%',
+  },
+  {
+    title: 'Driving',
+    emoji: '🚗',
+    top: '85%',
+    left: '20%',
+  },
+  {
+    title: 'Painting',
+    emoji: '🎨',
+    top: '40%',
+    left: '35%',
+  },
+  {
+    title: 'Cooking',
+    emoji: '🍳',
+    top: '35%',
+    left: '10%',
+  },
+  {
+    title: 'Reading',
+    emoji: '📚',
+    top: '45%',
+    left: '70%',
+  },
+  {
+    title: 'Weightlifting',
+    emoji: '🏋️',
+    top: '65%',
+    left: '5%',
+  },
+  {
+    title: 'Hiking',
+    emoji: '🥾',
+    top: '70%',
+    left: '45%',
+  },
+]
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
-};
+  return (
+    <div className='py-20 lg-py-28'>
+      <div className='container'>
+        <SectionHeader
+          eyebrow='About Me'
+          title='A Glipmse Into My World'
+          description='Learn more about me and what I do.'
+        />
+        <div className='mt-20 flex flex-col gap-8'>
+          <div className='grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3'>
+            <Card className='h-[320px] md:col-span-2 lg:col-span-1'>
+              <CardHeader
+                title='My Reads'
+                description='Explore the books shaping my perspective'
+              />
+              <div className='w-40 mx-auto mt-2 md:mt-0'>
+                <Image src={bookImage} alt='Book Cover' />
+              </div>
+            </Card>
+            <Card className='h-[320px] md:col-span-3 lg:col-span-2'>
+              <CardHeader
+                className=''
+                title='My Toolbox'
+                description='Explore the technologies and tools I use to create digital experiences.'
+              />
+              <ToolboxItems items={toolboxItems} className='' />
+              <ToolboxItems
+                items={toolboxItems}
+                className='mt-6'
+                itemsWrapperClassName='-translate-x-1/2'
+              />
+            </Card>
+          </div>
+          <div className='grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8'>
+            <Card className='h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2'>
+              <CardHeader
+                title='Beyond the code'
+                description='Explore my interests and hobbies beyond the digital realm'
+                className='px-6 py-6'
+              />
+              <div className='relative'>
+                {hobbies.map((hobby) => (
+                  <div
+                    key={hobby.title}
+                    className='inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute'
+                    style={{
+                      left: hobby.left,
+                      top: hobby.top,
+                    }}
+                  >
+                    <span className='font-medium text-gray-950'>
+                      {hobby.title}
+                    </span>
+                    <span>{hobby.emoji}</span>
+                  </div>
+                ))}
+              </div>
+            </Card>
+            <Card className='h-[320px] p-0 relative md:col-span-2 lg:col-span-1'>
+              <Image
+                className='h-full w-full object-cover object-left-top'
+                src={mapImage}
+                alt='Map'
+              />
+              <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[""] after:absolute after:inset-0 after:outline after:outline-2 after:outline-offset-2 after:outline-gray-950/30 after:rounded-full'>
+                <Image
+                  src={smileMemoji}
+                  alt='smiling Memoji'
+                  className='size-20'
+                />
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
